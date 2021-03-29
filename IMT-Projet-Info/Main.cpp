93ae6ccb451e543124c0c149b7a3011eca762256
Louis Delacour <louisdelacour01@gmail.com>
2021-03-29 13:14:54 +0200

    Test raylib

--- IMT-Projet-Info/Main.cpp
+++ IMT-Projet-Info/Main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
 
 using namespace std;
+#include <raylib.h>
 
-void main() {
-	cout << "Hello World !" << endl;
+
+
+int main() {
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);
+    Color color = BLACK;
+    const char* s1 = "Press Enter to start";
+
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(color);
+        
+        
+        if (IsKeyPressed(KEY_ENTER)) {
+            color = RED;
+            s1 = "Let's GO!!!";
+        }
+        DrawText(("%c", s1), 300, 200, 20, LIGHTGRAY);
+        EndDrawing();
+       
+        //----------------------------------------------------------------------------------
+    }
+
+    CloseWindow();
+
+    return 0;
 }
