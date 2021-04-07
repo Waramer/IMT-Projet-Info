4f134fd38c65adb6d31da0e90a9d47507832a386
Louis Delacour <louisdelacour01@gmail.com>
2021-04-07 12:17:34 +0200

    Ecran Settings + Higscores

--- IMT-Projet-Info/Ecran.cpp
+++ IMT-Projet-Info/Ecran.cpp
@@ -33,8 +33,10 @@ Ecran::Ecran()
         double y = GetScreenHeight();
         Vector2 pointeur = { x * 0.5, y * 0.5 };
 
-
+        
         Color color = BLACK;
+
+        //Titre de l'écran d'accueil
         const char* s0 = "ASTEROIDS";
         int tailleS0 = MeasureText(s0, 120);
 
@@ -46,6 +48,35 @@ Ecran::Ecran()
         OptionMenu options[4] = { option1, option2, option3, option4 };
         Color tabcolors[4] = { options[0].getColor(), options[1].getColor(), options[2].getColor(), options[3].getColor() };
         int res = 0;
+
+        //Titre de l'écran de réglages
+        const char* s1 = "SETTINGS";
+        int tailleS1 = MeasureText(s1, 120);
+
+        //options de l'écran de réglages
+        OptionMenu optionSettings1("FULL SCREEN : ", true);
+        OptionMenu optionSettings2("DIFFICULTY : ", false);
+        OptionMenu optionSettings3("MUSIC : ", false);
+        OptionMenu optionSettings4("RETURN TO MENU", false);
+        OptionMenu optionsSettings[4] = { optionSettings1, optionSettings2, optionSettings3, optionSettings4 };
+        Color tabColorsSettings[4] = { optionsSettings[0].getColor(),optionsSettings[1].getColor() ,optionsSettings[2].getColor(),optionsSettings[3].getColor() };
+
+        // Titre de l'écran des scores
+        const char* s2 = "HIGHSCORES";
+        int tailleS2 = MeasureText(s2, 120);
+
+
+        // choix options
+        const char* displayTab[2] = { "OFF","ON" };
+        const char* difficultyTab[3] = { "EASY","NORMAL", "HARD" };
+        const char* musicTab[2] = { "ON","OFF" };
+        int selectionSettings = 0;
+        int selectionDisplay = 0;
+        int selectionDifficulty = 0;
+        int selectionMusic=0;
+
+        
+
 
         //début de la fenêtre
         while (!WindowShouldClose())
@@ -87,9 +118,11 @@ Ecran::Ecran()
                     }
                     else if (selection == 1) {
                         // Ouvrir la page Highscores
+                        res = 3
                     }
                     else if (selection == 2) {
                         // Ouvrir la page Settings
+                        res = 4;
                     }
                     else if (selection == 3) {
                         // Quitter le jeu
@@ -184,6 +217,102 @@ Ecran::Ecran()
                 //afficher le pointeur
 
                 DrawPolyLines(pointeur, 3, 20, angle, WHITE);
+            }
+            else if (res == 3) {
+            // Page Highscores
+            BeginDrawing();
+            ClearBackground(color);
+
+            DrawText(("%c", s2), x / 2 - tailleS2 / 2, 100, 120, WHITE);
+            }
+            else if (res == 4) {
+
+            BeginDrawing();
+            ClearBackground(color);
+
+            //déplacement entre les différents menus avec les touches up et down
+            if (IsKeyPressed(KEY_DOWN) && selectionSettings < 3) {
+                tabColorsSettings[selectionSettings] = WHITE;
+                optionsSettings[selectionSettings].setSelection(false);
+
+                tabColorsSettings[selectionSettings + 1] = RED;
+                optionsSettings[selectionSettings + 1].setSelection(true);
+
+                selectionSettings++;
+            }
+
+            if (IsKeyPressed(KEY_UP) && selectionSettings > 0) {
+                tabColorsSettings[selectionSettings] = WHITE;
+                optionsSettings[selectionSettings].setSelection(false);
+
+                tabColorsSettings[selectionSettings - 1] = RED;
+                optionsSettings[selectionSettings - 1].setSelection(true);
+                selectionSettings--;
+            }
+
+            switch(selectionSettings){
+            case 0:
+                //DISPLAY
+                if (IsKeyPressed(KEY_RIGHT) && selectionDisplay < 1) {
+                    selectionDisplay++;
+                }
+
+                if (IsKeyPressed(KEY_LEFT) && selectionDisplay > 0) {
+                    selectionDisplay--;
+                }
+                if (selectionDisplay == 0) {
+                    if (IsKeyPressed(KEY_ENTER)) {
+                        ToggleFullscreen();
+                    }
+                    
+                }
+                if (selectionDisplay == 1) {
+                    if (IsKeyPressed(KEY_ENTER)) {
+                        ToggleFullscreen();
+                    }
+                    
+                }
+                break;
+            case 1:
+                // Difficulty
+                if (IsKeyPressed(KEY_RIGHT) && selectionDifficulty < 2) {
+                    selectionDifficulty++;
+                }
+
+                if (IsKeyPressed(KEY_LEFT) && selectionDifficulty > 0) {
+                    selectionDifficulty--;
+                }
+                break;
+            case 2:
+                // Music
+                if (IsKeyPressed(KEY_RIGHT) && selectionMusic < 1) {
+                    selectionMusic++;
+                }
+
+                if (IsKeyPressed(KEY_LEFT) && selectionMusic > 0) {
+                    selectionMusic--;
+                }
+                break;
+            case 3:
+                if (IsKeyPressed(KEY_ENTER)) {
+                    res = 0;
+                }
+                break;
+            default:
+                break;
+            }
+
+            //position des menus sur l'ecran
+            DrawText(("%c", s1), x / 2 - tailleS1 / 2, 100, 120, WHITE);
+            DrawText(optionSettings1.getString(), optionSettings1.getPosition(x), y * 0.35, 40, tabColorsSettings[0]);
+            DrawText(displayTab[selectionDisplay], optionSettings1.getPosition(x) + MeasureText(optionSettings1.getString(),40), y * 0.35, 40, GRAY);
+            DrawText(optionSettings2.getString(), optionSettings2.getPosition(x), y * 0.35 + y * 0.1, 40, tabColorsSettings[1]);
+            DrawText(difficultyTab[selectionDifficulty], optionSettings2.getPosition(x) +  MeasureText(optionSettings2.getString(), 40), y * 0.35 + y * 0.1, 40, GRAY);
+            DrawText(optionSettings3.getString(), optionSettings3.getPosition(x), y * 0.35 + y * 0.2, 40, tabColorsSettings[2]);
+            DrawText(musicTab[selectionMusic], optionSettings3.getPosition(x) +  MeasureText(optionSettings3.getString(), 40), y * 0.35 + y * 0.2, 40, GRAY);
+            DrawText(optionSettings4.getString(), optionSettings4.getPosition(x), y * 0.35 + y * 0.3, 40, tabColorsSettings[3]);
+            EndDrawing();
+
             }
 
         }
