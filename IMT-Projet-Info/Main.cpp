c29d2f41d7276068e2e2b6a1aa20df9ca897828d
Hugo Vanhille <hugovanhille59@orange.fr>
2021-04-07 09:40:18 +0200

    update

--- IMT-Projet-Info/Main.cpp
+++ IMT-Projet-Info/Main.cpp
@@ -3,6 +3,7 @@
 #include "Asteroid.h"
 #include "Point.h"
 #include <random>
+#include "physac.h"
 
 using namespace std;
 
@@ -23,9 +24,13 @@ void main() {
 	InitWindow(700, 500,"Ceci est une fenetre");
 	while (!WindowShouldClose()) { 
 		EnableCursor(); 
+		// Initialize physics and default physics bodies
+		InitPhysics();
+
 		BeginDrawing();
-		ClearBackground(RED);
-		DrawText("Vous etes un genie", 50, 40, 30, WHITE);
+		//ClearBackground(RED);
+		//DrawText("Vous etes un genie", 50, 40, 30, WHITE);
+		ClearBackground(WHITE);
 		EndDrawing();
 	}
 	CloseWindow();
